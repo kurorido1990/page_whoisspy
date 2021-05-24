749e9854bd6b10f02fc61ef9b0a688b22b690894
kurorido_lo <kurorido_lo@paradise-soft.com.tw>
2021-05-24 23:47:23 +0800

    xxx

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    publicPath: '/',
-    // publicPath: process.env.NODE_ENV === 'production'
-    // ? '/page_whoisspy/'
-    // : '/',
+    // publicPath: '/',
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/page_whoisspy/'
+    : '/',
     outputDir: 'dist',
     lintOnSave: process.env.NODE_ENV === 'development',
     productionSourceMap: false,
